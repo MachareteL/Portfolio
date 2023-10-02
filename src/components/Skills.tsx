127381382b41ddedca8fe73aebe3beabdd661f9f
MachareteL <macharetelucas@gmail.com>
2023-10-02 13:52:49 -0300

    feat: basic design built

---
+++ src/components/Skills.tsx
@@ -0,0 +1,26 @@
+export default function Skills() {
+  return (
+    <div className="mb-10 hidden grid-cols-2 gap-8 md:grid">
+      <div className="title place-self-end">
+        <h1 className="text-3xl">Front-end</h1>
+        <h2 className="text-xl font-medium text-wine">skills</h2>
+        <div className="grid grid-cols-2 gap-2">
+          <h1>placeholder</h1>
+          <h1>placeholder</h1>
+          <h1>placeholder</h1>
+          <h1>placeholder</h1>
+        </div>
+      </div>
+      <div className="title place-self-start">
+        <h1 className="text-3xl">Back-end</h1>
+        <h2 className="text-xl font-medium text-wine">skills</h2>
+        <div className="grid grid-cols-2 gap-2">
+          <h1>placeholder</h1>
+          <h1>placeholder</h1>
+          <h1>placeholder</h1>
+          <h1>placeholder</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
