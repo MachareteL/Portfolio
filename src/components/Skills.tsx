7e652207274f5259491de11f3aab25f320adbd3a
macharetel <macharetelucas@gmail.com>
2023-11-15 08:39:36 -0300

    feat: technologies became tooltips

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,24 +1,28 @@
 export default function Skills() {
+  const frontendTech = ["React js", "Vue js", "Tailwind", "Next js"]
+  const backendTech = ["Javascript", "Java", "C#", "Python"]
   return (
     <div className="mb-10 hidden grid-cols-2 gap-8 md:grid">
       <div className="title place-self-end">
         <h1 className="text-3xl">Front-end</h1>
         <h2 className="text-xl font-medium text-wine">skills</h2>
         <div className="grid grid-cols-2 gap-2">
-          <h1>placeholder</h1>
-          <h1>placeholder</h1>
-          <h1>placeholder</h1>
-          <h1>placeholder</h1>
+          {frontendTech.map((tech) => (
+            <h1 className="rounded-full bg-gray-800 px-2 text-center text-white">
+              {tech}
+            </h1>
+          ))}
         </div>
       </div>
       <div className="title place-self-start">
         <h1 className="text-3xl">Back-end</h1>
         <h2 className="text-xl font-medium text-wine">skills</h2>
         <div className="grid grid-cols-2 gap-2">
-          <h1>placeholder</h1>
-          <h1>placeholder</h1>
-          <h1>placeholder</h1>
-          <h1>placeholder</h1>
+          {backendTech.map((tech) => (
+            <h1 className="rounded-full bg-gray-800 px-2 text-center text-white">
+              {tech}
+            </h1>
+          ))}
         </div>
       </div>
     </div>
