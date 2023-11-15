a5c5f6c554ca6309d8b107045eaf6cc091b55e93
macharetel <macharetelucas@gmail.com>
2023-11-15 13:00:14 -0300

    feat: swippper camel case switch

--- src/components/swipperCard.tsx
+++ src/components/swipperCard.tsx
@@ -3,7 +3,7 @@ export function SwipperCard({ imgsrc }: { imgsrc: string }) {
     <div className="relative mb-10 w-full cursor-pointer overflow-hidden rounded-2xl bg-red-500 pb-14 pt-40 shadow-md shadow-black md:pt-72">
       <img
         src={imgsrc}
-        alt=""
+        alt="src"
         className="absolute left-0 top-0 h-full w-full object-cover object-center transition-all hover:scale-105"
       />
     </div>
